a25687ba78c072ad8dbbf3b13acc1eaa4c9a05b5
Felix Lin <felixlin77@gmail.com>
2025-02-04 17:56:44 +1100

    Initial Upload

---
+++ index.js
@@ -0,0 +1,82 @@
+const socket = new WebSocket("ws://127.0.0.1:3030");
+
+function append_word(word) {
+    const word_list = document.getElementById("word-list");
+    const list_item = document.createElement("li");
+    list_item.textContent = word;
+    word_list.appendChild(list_item);
+}
+
+function update_game_state(data) {
+    error_popup = document.getElementById("error-popup");
+    // remove any prev error popups from screen
+    error_popup.style.display = "none";
+
+    if (data.error) {
+        error_popup.textContent = data.error;
+        error_popup.style.display = "block";
+
+        // fade out the error popup
+        setTimeout(() => {
+            error_popup.style.transition = "opacity 1s ease-out";
+            error_popup.style.opacity = "0";
+
+            setTimeout(() => {
+                error_popup.style.display = "none";
+                error_popup.style.opacity = "1";
+            }, 1000);
+        }, 1500);
+        return;
+    }
+
+    document.getElementById("prev-word").innerText = "Previous Word: " + data.new_word;
+    append_word(data.new_word);
+    
+    word_list.scrollTop = word_list.scrollHeight;
+}
+
+socket.onopen = function (event) {
+    console.log("Connected");
+
+    try {
+        const data = JSON.parse(event.data);
+        data.words.forEach(word => {
+            append_word(word);
+        });
+
+        document.getElementById("prev-word").innerText = "Previous Word: " + data.new_word;
+    } catch (error) {
+        console.error("Failed to parse server msg: ", error);
+    }
+};
+
+socket.onclose = function (event) {
+    console.log("Connection closed");
+}
+
+socket.onerror = function (error) {
+    console.log("Error: ", error);
+}
+
+socket.onmessage = function(event) {
+    try {
+        const data = JSON.parse(event.data);
+        update_game_state(data);
+    } catch (error) {
+        console.error("Failed to parse server msg: ", error);
+    }
+};
+
+document.getElementById("submit-word").addEventListener("click", function () {
+    const word_input = document.getElementById("word-input");
+    socket.send(word_input.value);
+    word_input.value = "";
+});
+
+document.getElementById("word-input").addEventListener("keypress", function(e) {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        document.getElementById("submit-word").click();
+    }
+});
+
---
+++ src/main.rs
@@ -0,0 +1,101 @@
+use std::{collections::{HashMap, HashSet}, fs::read_to_string, sync::{Arc, Mutex}};
+use ws::{Handler, Sender, Result, Message, listen};
+
+struct Game {
+    words: Vec<String>,
+    prev_submitter: Option<Sender>,
+    dict: HashSet<String>,
+}
+
+struct Server {
+    out: Sender,
+    game: Arc<Mutex<Game>>,
+}
+
+impl Handler for Server {
+    fn on_open(&mut self, _: ws::Handshake) -> Result<()> {
+        let game = self.game.lock().unwrap();
+        let res = serde_json::json!({
+            "words": game.words,
+            "new_word": game.words.last().expect("there is always a word, 'a' to begin with"),
+        });
+
+        self.out.send(res.to_string())
+    }
+
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        let mut game = self.game.lock().unwrap();
+        let word = msg.as_text()?.trim().to_lowercase();
+
+        fn error_check(game: &Game, word: &String, out: &Sender) -> Option<String> {
+            // check the consecutive submission rule is followed
+            if let Some(prev_submitter) = &game.prev_submitter {
+                if prev_submitter == out {
+                    return Some(String::from("You cannot submit twice in a row"))
+                }
+            }
+            
+            // check the word-chain rule is followed
+            if let Some(prev_word) = game.words.last() {
+                if prev_word.chars().last().unwrap() != word.chars().next().unwrap() {
+                    return Some(format!("Your word, '{word}', does not start with the last letter of the previous word, {prev_word}"))
+                }
+            }
+            
+            // validate the word itself
+            if word.is_empty() {
+                Some(String::from("Word cannot be empty"))
+            } else if game.words.contains(word) {
+                Some(format!("Your word, '{word}', has already been used in the chain"))
+            } else if !game.dict.contains(word) {
+                Some(format!("Your word, '{word}', is not a valid English word"))
+            } else {
+                None
+            }
+        }
+
+        // if any errors, send error msg to this socket conn only
+        if let Some(e) = error_check(&game, &word, &self.out) {
+            let error_msg = serde_json::json!({
+                "error": format!("Error: {e}!"),
+            });
+            
+            return self.out.send(error_msg.to_string());
+        }
+
+        // otherwise update game state and broadcast it (JSONified) to 
+        // all socket conns
+        game.words.push(word.clone());
+        game.prev_submitter = Some(self.out.clone());
+        
+        let res = serde_json::json!({
+            "words": game.words,
+            "new_word": word,
+        });
+        
+        self.out.broadcast(res.to_string())
+    }
+}
+
+fn main() -> Result<()> {
+    // initialise Eng dictionary
+    let dict = read_to_string("src/words.json").expect("Failed to read words.json");
+    let dict: HashMap<String, i32> = serde_json::from_str(&dict)
+        .expect("Failed to parse words JSON");
+    let dict: HashSet<String> = dict.keys().cloned().collect();
+
+    // Arc<Mutex> for concurrency
+    let game = Arc::new(Mutex::new(Game {
+        words: vec![String::from("a")],
+        prev_submitter: None,
+        dict,
+    }));
+
+    println!("Hello, world!");
+
+    // listen on local port 3030
+    listen("127.0.0.1:3030", |out| Server {
+        out,
+        game: game.clone(),
+    })
+}
